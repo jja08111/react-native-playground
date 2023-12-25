3cb0a34936a050a96e63e326a0d76ca11bac2d4c
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-12-25 14:33:10 +0900

    Move App.tsx into src

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
-import TodoScreen from './src/screen/TodoScreen';
+import TodoListScreen from './screen/TodoListScreen';
 
 const Stack = createStackNavigator();
 
@@ -9,7 +9,7 @@ function App(): React.JSX.Element {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Todo" component={TodoScreen} />
+        <Stack.Screen name="Todo" component={TodoListScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screen/TodoListScreen/index.tsx
+++ src/screen/TodoListScreen/index.tsx
@@ -1,6 +1,6 @@
 import {Text} from 'react-native';
 
-export default function TodoScreen() {
+export default function () {
   return (
     <>
       <Text>hi</Text>
