88c87862f8312b3ee70ed5a0d23e93d9d9a198c6
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-12-25 14:31:02 +0900

    Setup navigator

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 import TodoScreen from './src/screen/TodoScreen';
 
 const Stack = createStackNavigator();
 
 function App(): React.JSX.Element {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Todo" component={TodoScreen} />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Todo" component={TodoScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
